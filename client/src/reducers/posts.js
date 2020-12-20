5e83ba2967825f6ef2fd4dcc747c55b8839f2572
Adam Morgan <adamcodes716@gmail.com>
2020-12-20 11:30:49 -0500

    pulling data from api

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -2,7 +2,7 @@ export default (posts = [], action) => {
 
     switch (posts= [], action.type) {
         case 'FETCH_ALL':
-            return posts;
+            return action.payload;
         case 'CREATE': 
             return posts;
         default: 
