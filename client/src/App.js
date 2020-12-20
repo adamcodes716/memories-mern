b6005801a82f3c1469b68c2f42282e1985ed1d42
Adam Morgan <adamcodes716@gmail.com>
2020-12-20 13:24:40 -0500

    first course completed

--- client/src/App.js
+++ client/src/App.js
@@ -19,8 +19,9 @@ const App = () => {
        <Container maxwidth="lg">
             <AppBar className={classes.appBar} postion="static" color="inherit">
                 <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60" />
+                <img className={classes.image} src={memories} alt="memories" height="60px" />
             </AppBar>
+            <br /><br /><br /><br /><br /><br /><br />
             <Grow in>
                 <Grid container justify="space-between" alignItems="stretch" spacing={3}>
                     <Grid item xs={12} sm={7}>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -4,15 +4,16 @@ import PostMessage from '../models/postMessage.js';
 export const getPosts = async (req, res) => {
     try{
         const postMessages = await PostMessage.find();
-        res.status(200).json(postMessages);
+        res.status(200).json(PostMessages);
     } catch (error) {
        res.status(404).json( {message: error.message })
     }
  }
 
  export const createPost = async (req, res) => {
+    console.log (" in the create post");
      const post = req.body;
-     const newPost = newPostMessage(post);
+     const newPost = new PostMessage(post);
      try{
         await newPost.save();
         res.status(201).json(newPost);
