6fbc4b21f189a1b69dd66676100400b6081cdc02
Adam Morgan <adamcodes716@gmail.com>
2020-12-21 15:49:35 -0500

    updating post

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,23 +1,40 @@
-import React, {useState } from 'react';
+import React, {useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper} from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import useStyles from './styles';
-import { createPost } from '../../actions/posts';
+import { createPost, updatePost } from '../../actions/posts';
 
-const Form = () => {
-    const[postData, setPostData] = 
-    useState({creator: '', title : '', message: '', tags: '', selectedFile: ''
-    })
- const classes = useStyles();
- const dispatch = useDispatch();
- const handleSubmit = (e) => {
+// get the current ID of the post we are on
+
+const Form = ({ currentId, setCurrentId }) => {
+const[postData, setPostData] = useState({creator: '', title : '', message: '', tags: '', selectedFile: ''});
+const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId): null);   
+const classes = useStyles();
+const dispatch = useDispatch();
+
+useEffect(() => {
+   console.log ("post changed!!!!");
+   if (post) setPostData(post);
+}, [post]);
+
+
+const handleSubmit = (e) => {
      e.preventDefault();
+     console.log ("currentId", currentId);
+    if (currentId) {  // don't dispatch create if we have an id
+        dispatch(updatePost(currentId, postData));
+    } else {
      dispatch(createPost(postData));
- }
+    } 
+    clear();
+}
 
  const clear = () => {
+     console.log ("clearing");
+     setCurrentId(null);
+     setPostData({ creator: '', title : '', message: '', tags: '', selectedFile: '' })
  }
 
 return (
